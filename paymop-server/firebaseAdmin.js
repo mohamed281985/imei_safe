baf4ad2f60910d5569a2b3141a5c3dded8a30c21
mohamed281985 <maly5424@gmail .com>
2026-06-19 03:54:31 +0300

    pkjkjggtyuty

--- paymop-server/firebaseAdmin.js
+++ paymop-server/firebaseAdmin.js
@@ -1,20 +1,12 @@
 import admin from 'firebase-admin';
 
-const projectId = process.env.FIREBASE_PROJECT_ID;
-const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-const privateKeyRaw = process.env.FIREBASE_PRIVATE_KEY;
-
-if (!admin.apps.length) {
-  if (!projectId || !clientEmail || !privateKeyRaw) {
-    throw new Error('Missing Firebase Admin environment variables: FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY');
-  }
-
+if (!admin.apps?.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId,
-      clientEmail,
-      privateKey: privateKeyRaw.replace(/\\n/g, '\n')
-    })
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
   });
 }
 
